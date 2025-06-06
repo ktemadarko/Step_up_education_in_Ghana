75fd4bbef2d9e623417f8d180bbaf91946914933
Ama Owusu-Darko <39912040+ktemadarko@users.noreply.github.com>
2025-06-06 20:59:57 +0000

    Added more documentation to app.R

--- app.R
+++ app.R
@@ -1,11 +1,36 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#---------------------------------------------------------------------------------------------------------------------
+# Title: Step up education in Ghana, understanding the distribution of education facilities in Ghana.
+# Created By: Ama Korantema Owusu-Darko
+# Last Edited: 6th-June-2025 
+#---------------------------------------------------------------------------------------------------------------------
+# Description: 
+#---------------------------------------------------------------------------------------------------------------------
+#	This R code generates a Shiny dashboard with two tabs.
+# The first tab Educational facilities create maps that display the different types of education facilities in Ghana.
+#	
+# The second tab displays a bar graph that shows the Percentage of a select school indicator eg. (School dropout) by 
+# Gender and by Region in Ghana.
+	
+# Sections:
+#	- Step 1. Change working directory Downloads folder
+#	- Step 2. Load input dta file 
+#	- Step 3: Create a demographic table using the distribution of trainees by age and gender
+	
+#	- Step 4: Create a bar graph of the age_categorical variable
+#	- Step 5: Create a bar graph of covid_anxious_cvd variable
+#	- Step 6. Create a two-by-two contingency table of 
+
+# Input data location: ./Owusu-Darko_Ama/01_raw_data
+
+# Input data files: - covid_mental_health.dta
+#---------------------------------------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# Load required R packages
+#---------------------------------------------------------------------------------------------------------------------
+
 library(shiny)
 library(tidyverse)
 library(magrittr)
@@ -13,60 +38,98 @@ library(tmap)
 library(sf)
 library(DT)
 
+#---------------------------------------------------------------------------------------------------------------------
+# Loading Geospatial data
+#---------------------------------------------------------------------------------------------------------------------
+# Load geospatial shapfiles located in the data folder of this repository
 gh_adm2<-read_sf("data/gha_admin_shp/gha_admbnda_adm2_gss_20210308.shp")
 
 
-#load edu facilities
+# load point data on the location of education facilities in Ghana located in the data folder of this repository
 edu_facilities<-read_sf("data/hotosm_gha_edu_facilities_points_shp/hotosm_gha_education_facilities_points.shp")
 
+
+#---------------------------------------------------------------------------------------------------------------------
+# Data Wrangling
+#---------------------------------------------------------------------------------------------------------------------
+
+# Change the values of the `amenity` to title case (start with a capital letter)
 edu_facilities$amenity<-str_to_title(edu_facilities$amenity)
 
+# Streamline the categories in the amenity column by replacing all instance of "Basic|Preparatory|Primary" to "Primary"
 edu_facilities$amenity<-if_else((str_detect(edu_facilities$name,
                                             "Basic|Preparatory|Primary")),"Primary",edu_facilities$amenity)
 
 
+
+# Removing rows that have NA in the name column or NA in the amenity column from the data set 
 na_name=function(x){
   x|>
     mutate(across(c(amenity),factor))|>
     filter(!is.na(name),
            !is.na(amenity))}
 
-edu_new<-na_name(edu_facilities)
-edu_new%<>%
+
+# Removing empty levels from the factors
+edu_new <- na_name(edu_facilities)
+edu_new %<>%
   mutate(across(c(amenity),droplevels))
 
 
-tmap_mode("view")
-choose_region=function(x,y){
+# tmap rpackage for creating the maps
+tmap_mode("view") 
+
+#-------------------------------------------------------------------------------------------------------------------------------
+# Creating the `choose_region()` function that will be used to select a region in the shiny app 
+#-------------------------------------------------------------------------------------------------------------------------------
+choose_region = function(x,y){
   filter(x,ADM1_EN==y)
 }
+
+#-------------------------------------------------------------------------------------------------------------------------------
+# Creating the `choose_school()` function that will be used to select a type of school eg. Primary or Secondary in the shiny app 
+#-------------------------------------------------------------------------------------------------------------------------------
 choose_school=function(x){
   filter(edu_new,amenity==x)
   #filter(y) if region was in edu_new data set can filter by coordinate bounding box
 }
 
-choices_reg=factor(gh_adm2$ADM1_EN)
+# Creating a choices vector which will be used to select a region in the shiny app
+choices_reg = factor(gh_adm2$ADM1_EN)
 
 
+#---------------------------------------------------------------------------------------------------------------------------------
+# Load school attendance data and generate three new columns below
+#   * Percent_dropout (Divide the column Dropped_out_of_School by the column Sum_of_Respondents_(3_years_and_above)
+#   * Percent_Currently_in_School (Divide the column Currently_Attending_School by the column Sum_of_Respondents_(3_years_and_above)
+#   * Percent_Never_Attended_School (Divide the column Never_Attended_School by the column Sum_of_Respondents_(3_years_and_above)
+#----------------------------------------------------------------------------------------------------------------------------------
 
-#load Ghana school attendance data 3 years and above from the Ghana 2021 Census Literacy reports
+# Load Ghana school attendance data 3 years and above from the Ghana 2021 Census Literacy reports located in the data folder of this repository
 gh_school_attend=read_csv("data/GHA_2021_Census_Pop_3yrs_school_attendance.csv")
 
 gh_school_attend%<>%
-  mutate(Percent_dropout=round(((gh_school_attend$Dropped_out_of_School/
+  mutate(Percent_dropout = round(((gh_school_attend$Dropped_out_of_School/
                                    gh_school_attend$`Sum_of_Respondents_(3_years_and_above)`)
                                 *100),2),
-         Percent_Currently_in_School=round(((gh_school_attend$Currently_Attending_School/
+         Percent_Currently_in_School = round(((gh_school_attend$Currently_Attending_School/
                                                gh_school_attend$`Sum_of_Respondents_(3_years_and_above)`)
                                             *100),2),
          Percent_Never_Attended_School=round(((gh_school_attend$Never_Attended_School/
                                                  gh_school_attend$`Sum_of_Respondents_(3_years_and_above)`)
                                               *100),2))
 
-#rename Region to ADM1_EN so can share common functions
+
+#----------------------------------------------------------------------------------------------------------------------------------
+# Rename Region to ADM1_EN so that the naming is standard across functions
+#----------------------------------------------------------------------------------------------------------------------------------
 gh_school_attend%<>%
   mutate(across(c(ADM1_EN,Gender,Locality),factor))
 
+
+#----------------------------------------------------------------------------------------------------------------------------------
+# Create the UI for the shiny app
+#----------------------------------------------------------------------------------------------------------------------------------
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Step up Education in Ghana",
@@ -107,7 +170,9 @@ ui <- navbarPage("Step up Education in Ghana",
 # Show a plot of the generated distribution
 
 
-
+#----------------------------------------------------------------------------------------------------------------------------------
+# Create the Server for the shiny app
+#----------------------------------------------------------------------------------------------------------------------------------
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
